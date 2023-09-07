ae4faba9459c0f4ddc59c61b9ee812a7c5cc0475
Geass2116 <113455185+Geass2116@users.noreply.github.com>
2023-09-07 21:13:36 +0530

    change base url

--- client/src/scenes/loginPage/Form.jsx
+++ client/src/scenes/loginPage/Form.jsx
@@ -63,7 +63,8 @@ const Form = () => {
     formData.append("picturePath", values.picture.name);
 
     const savedUserResponse = await fetch(
-      "http://localhost:3001/auth/register",
+      "https://socialmedia-crud.onrender.com/auth/register",
+      // "http://localhost:3001/auth/register",
       {
         method: "POST",
         body: formData,
@@ -78,7 +79,8 @@ const Form = () => {
   };
 
   const login = async (values, onSubmitProps) => {
-    const loggedInResponse = await fetch("http://localhost:3001/auth/login", {
+    const loggedInResponse = await fetch("https://socialmedia-crud.onrender.com/auth/login", {
+    // const loggedInResponse = await fetch("http://localhost:3001/auth/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(values),
